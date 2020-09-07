33698723b1f0a6c911431979c808ad11e11b00e1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-07 16:55:40 +0200

    Exercice enfant.js

---
+++ chapitre_1/js/enfant.js
@@ -0,0 +1,36 @@
+/*
+ Exercice : afficher l'enfant d'un objet du DOM
+*/
+
+// Affiche un enfant d'un objet du DOM
+// Le paramètre noeud est l'objet du DOM
+// Le paramètre indice est l'indice de l'enfant à afficher
+function afficherEnfant(noeud, indice) {
+    if (noeud.nodeType === document.TEXT_NODE) {
+        console.error("Type de noeud incorrect");
+    } else {
+        if (indice < 0) {
+            console.error("Indice incorrect");
+        } else if (indice > noeud.childNodes.length) {
+            console.error("Indice incorrect");
+        } else {
+            console.log(noeud.childNodes[indice]);
+        }
+    }
+    
+}
+
+// Doit afficher le noeud h1
+afficherEnfant(document.body, 1);
+
+// Doit afficher l'erreur "Indice incorrect"
+// L'indice demandé est négatif
+afficherEnfant(document.body, -1);
+
+// Doit afficher l'erreur "Indice incorrect"
+// Le noeud body a moins de 9 noeuds enfants
+afficherEnfant(document.body, 8);
+
+// Doit afficher l'erreur "Type de noeud incorrect"
+// Le premier noeud enfant de body est textuel et n'a donc pas d'enfants
+afficherEnfant(document.body.childNodes[0], 0);
