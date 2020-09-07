17f4f97f2663650c5baa5f0690f84a519bb5e334
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-07 17:47:30 +0200

    Préparation de comptage des éléments

---
+++ chapitre_2/js/reve.js
@@ -0,0 +1,6 @@
+// TODO : écrire la fonction compterElements
+
+console.log(compterElements("p")); // Doit afficher 4
+console.log(compterElements(".adjectif")); // Doit afficher 3
+console.log(compterElements("p .adjectif")); // Doit afficher 3
+console.log(compterElements("p > .adjectif")); // Doit afficher 2
