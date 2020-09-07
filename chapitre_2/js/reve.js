fe49350b4bd00d5a938a59b06eb8942c1972e516
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-07 17:50:47 +0200

    Préparation de la fonction

--- chapitre_2/js/reve.js
+++ chapitre_2/js/reve.js
@@ -1,4 +1,8 @@
 // TODO : écrire la fonction compterElements
+function compterElements(noeud) {
+    console.log(document.querySelector(noeud).length);
+}
+
 
 console.log(compterElements("p")); // Doit afficher 4
 console.log(compterElements(".adjectif")); // Doit afficher 3
