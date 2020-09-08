391e622b4c80241a5bc480889f35b5630755f3da
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-08 17:19:03 +0200

    Attributs et classes

--- chapitre_3/js/cours.js
+++ chapitre_3/js/cours.js
@@ -5,4 +5,14 @@ document.getElementById("langages").innerHTML += '<li id="c">C</li>';
 // document.getElementById("langages").innerHTML = "";
 
 // Modification du premier titre
-document.querySelector(".debut").textContent += " de programmation";
+document.querySelector("h1").textContent += " de programmation";
+
+// Ajout de l'attribut "id" du premier titre
+// document.querySelector("h1").setAttribute("id", "titre");
+// Autre manière
+document.querySelector("h1").id = "titre";
+
+let titreElt = document.querySelector("h1");
+titreElt.classList.remove("debut"); // Suppression de la classe debut
+titreElt.classList.add("titre");    // Ajout de la classe titre
+console.log(titreElt);
