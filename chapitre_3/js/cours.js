acbcc1e6c1122abc9901ecd8f23070d7fd2a0ad4
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-08 17:12:18 +0200

    innerHTML et textContent

---
+++ chapitre_3/js/cours.js
@@ -0,0 +1,8 @@
+// Ajout d'un langage
+document.getElementById("langages").innerHTML += '<li id="c">C</li>';
+
+// Suppression du contenu
+// document.getElementById("langages").innerHTML = "";
+
+// Modification du premier titre
+document.querySelector(".debut").textContent += " de programmation";
