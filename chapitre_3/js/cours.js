e765e79df5dbba6270049e5387ccb8a68b31c3a1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-08 17:40:28 +0200

    Ajouter un noeud

--- chapitre_3/js/cours.js
+++ chapitre_3/js/cours.js
@@ -16,3 +16,53 @@ let titreElt = document.querySelector("h1");
 titreElt.classList.remove("debut"); // Suppression de la classe debut
 titreElt.classList.add("titre");    // Ajout de la classe titre
 console.log(titreElt);
+
+
+/* Ajouter un élément */
+
+// Création d'un élément
+let pythonElt = document.createElement("li");
+
+// Ajout de son identifiant
+pythonElt.id = "python";
+
+// Ajout de son contenu textuel
+pythonElt.textContent = "Python";
+
+// Insertion du nouvel élément
+document.getElementById("langages").appendChild(pythonElt);
+
+// Création d'un autre élément
+let div = document.createElement("div");
+div.id = "uneDiv";
+div.style.width = "300px";
+div.style.height = "300px";
+div.style.border = "1px solid #000";
+div.style.display = "flex";
+div.style.justifyContent = "center";
+div.style.alignItems = "center";
+div.innerHTML = "<h2>Nouveau contenu !</h2>";
+document.getElementById("contenu").appendChild(div);
+console.log(div);
+
+// Création d'un autre élément avec createTextNode
+let rubyElt = document.createElement("li");
+rubyElt.id = "ruby";
+rubyElt.appendChild(document.createTextNode("Ruby"));
+document.getElementById("langages").appendChild(rubyElt);
+
+// Ajout du langage Perl avant le langage PHP
+let perlElt = document.createElement("li");
+perlElt.id = "Perl";
+perlElt.textContent = "Perl";
+document.getElementById("langages").insertBefore(perlElt, document.getElementById("php"));
+
+// Ajout du langage JavaScript au début de la liste
+document.getElementById("langages").insertAdjacentHTML("afterbegin", '<li id="javascript">JavaScript</li>');
+/* 
+    beforebegin : Avant l'élément cible.
+    afterbegin : avant le premier enfant de l'élément cible.
+    beforeend : après le dernier enfant de l'élément cible.
+    afterend : après l'élémnet cible.
+*/
+
