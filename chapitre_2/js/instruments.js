aec591b8376f148ca67160dd5723e141e2ec9684
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-08 17:04:08 +0200

    Correction exercice instruments

--- chapitre_2/js/instruments.js
+++ chapitre_2/js/instruments.js
@@ -4,5 +4,18 @@ function infosLiens() {
     console.log("Cible du premier lien : " + liens[0].href);
     console.log("Cible du dernier lien : " + liens[liens.length - 1].href);
 }
+// infosLiens();
 
-infosLiens();
+function possede(id, classe) {
+    let instrument = document.getElementById(id);
+    if (instrument !== null) {
+        console.log(instrument.classList.contains(classe));
+    } else {
+        console.error("Aucun élément ne possède l'identifiant " + id);
+    }
+}
+
+possede("saxophone", "bois"); // Doit afficher true
+possede("saxophone", "cuivre"); // Doit afficher false
+possede("trompette", "cuivre"); // Doit afficher true
+possede("contrebasse", "cordes"); // Doit afficher une erreur
