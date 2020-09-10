211cb94dfe82307a116dcb6d16a56e18dc7a3f3b
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-10 17:22:15 +0200

    Changement de couleur

---
+++ chapitre_5/js/couleurs.js
@@ -0,0 +1,24 @@
+let paras = document.getElementsByTagName('div');
+
+function changerCouleur(e) {
+    for (let i = 0; i < paras.length; i++) {
+        switch (String.fromCharCode(e.charCode)) {
+            case "r":
+                paras[i].style.backgroundColor = "red";
+                break;
+            case "j":
+                paras[i].style.backgroundColor = "yellow";
+                break;
+            case "v":
+                paras[i].style.backgroundColor = "green";
+                paras[i].style.color = "white";
+                break;
+            case "b":
+                paras[i].style.backgroundColor = "blue";
+                paras[i].style.color = "white";
+                break;
+        }
+    }
+}
+
+document.addEventListener('keypress', changerCouleur);
