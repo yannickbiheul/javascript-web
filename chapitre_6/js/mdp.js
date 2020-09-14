092a4d8272739089a9b92a742056be0438300d09
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-14 08:01:35 +0200

    Validation vérification mot de passe

--- chapitre_6/js/mdp.js
+++ chapitre_6/js/mdp.js
@@ -4,39 +4,52 @@ let mdp2 = document.getElementById("mdp2");
 let infoMdp = document.getElementById("infoMdp");
 let form = document.querySelector("form");
 let submit = form.elements[2];
-let valeurMdp1 = "";
+let userMdp = "";
+let userMdpConfirm = "";
 
 // Mise en forme de infoMdp
 infoMdp.style.color = "white";
 infoMdp.style.width = "300px";
+infoMdp.style.height = "25px";
 infoMdp.style.display = "flex";
 infoMdp.style.justifyContent = "center";
 infoMdp.style.alignItems = "center";
 
-// Valider les données du 1er mot de passe
-mdp1.addEventListener("input", function(e) {
-    valeurMdp1 = e.target.value;
-    if (valeurMdp1.length < 6) {
-        infoMdp.style.backgroundColor = "red";
-        infoMdp.textContent = "Mot de passe : Trop court !";
-    } else {
-        infoMdp.style.backgroundColor = "green";
-        infoMdp.textContent = "Mot de passe : Bonne taille !";
-    }
-    return valeurMdp1;
-})
-console.log(valeurMdp1);
 
-// Valider les données du 2ème mot de passe
-mdp2.addEventListener("input", function (e) {
-    let valeurMdp2 = e.target.value;
-    if (valeurMdp2.length < 6) {
+// Vérification des mots de passe
+mdp1.addEventListener("input", function(e) {
+    if (e.target.value.length < 6) {
         infoMdp.style.backgroundColor = "red";
-        infoMdp.textContent = "Confirmation : Trop court !";
+        infoMdp.textContent = "Trop court !";
     } else {
         infoMdp.style.backgroundColor = "green";
-        infoMdp.textContent = "Confirmation : Bonne taille !";
+        infoMdp.textContent = "Correct !";
+        userMdp = e.target.value;
     }
+})
+
+mdp2.addEventListener("input", function(e) {
+    if (e.target.value === userMdp) {
+        infoMdp.style.backgroundColor = "green";
+        infoMdp.textContent = "Confirmé !";
+        userMdpConfirm = e.target.value;
+    } else {
+        infoMdp.style.backgroundColor = "red";
+        infoMdp.textContent = "Non confirmé !";
+    }
+})
+
+form.addEventListener("submit", function(e) {
+    if (userMdp === userMdpConfirm) {
+        infoMdp.style.backgroundColor = "green";
+        infoMdp.textContent = "Mot de passe confirmé !";
+    } else {
+        infoMdp.style.backgroundColor = "red";
+        infoMdp.textContent = "Mot de passe non confirmé !";
+    }
+    e.preventDefault();
+    mdp1.value = "";
+    mdp2.value = "";
 })
 
 
