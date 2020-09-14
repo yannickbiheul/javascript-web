57e41951adbc9aa7b4f0c3917227533091958eff
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-14 08:04:00 +0200

    got

---
+++ chapitre_6/js/got.js
@@ -0,0 +1,35 @@
+// Liste de quelques maisons de Game of Thrones. Chaque maison a un code et un nom
+var maisons = [
+    {
+        code: "ST",
+        nom: "Stark"
+    },
+    {
+        code: "LA",
+        nom: "Lannister"
+    },
+    {
+        code: "BA",
+        nom: "Baratheon"
+    },
+    {
+        code: "TA",
+        nom: "Targaryen"
+    }
+];
+
+// Renvoie un tableau contenant quelques personnages d'une maison
+function getPersonnages(codeMaison) {
+    switch (codeMaison) {
+        case "ST":
+            return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+        case "LA":
+            return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+        case "BA":
+            return ["Robert", "Stannis", "Renly"];
+        case "TA":
+            return ["Aerys", "Daenerys", "Viserys"];
+        default:
+            return [];
+    }
+}
