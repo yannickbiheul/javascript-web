97b321e92c76365b8c59da8bf7476aa946ea6e40
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-14 09:59:41 +0200

    Pays 1er essai

--- chapitre_6/js/pays.js
+++ chapitre_6/js/pays.js
@@ -19,3 +19,20 @@ var listePays = [
     "Autriche",
     "Azerbaïdjan"
 ];
+
+let inputPays = document.getElementById("pays");
+let sugg = document.getElementById("suggestions");
+
+inputPays.addEventListener("input", function(e) {
+    for (let i = 0; i < listePays.length; i++) {
+        for (let p = 0; p <= listePays[i].length; p++) {
+            if (e.target.value === listePays[i][p]) {
+                sugg.innerHTML += listePays[i] + "<br>";
+            }
+        }
+    }
+})
+
+
+
+
