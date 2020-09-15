75db1697e8029c3a2ec6abf3cf10ea24b087e458
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-15 09:56:33 +0200

    Déplacer un bloc

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -1,7 +1,3 @@
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-document.body.style.alignItems = "center";
-document.body.style.height = "100vh";
 
 let compteurElt = document.getElementById("compteur");
 
@@ -23,3 +19,29 @@ function diminuerCompteur() {
 
 // Appelle la fonction diminuerCompteur toutes les secondes (1000 millisecondes)
 let intervalId = setInterval(diminuerCompteur, 1000);
+
+let bloc = document.getElementById("bloc");
+let cadre = document.getElementById("cadre");
+let vitesse = 7;    // Valeur du déplacement en pixels
+let largeurBloc = parseFloat(getComputedStyle(bloc).width);
+let animationId = null; // Identifiant de l'animation
+
+// Déplace le bloc sur sa gauche jusqu'au bord du cadre
+function deplacerBloc() {
+    // Conversion en nombre de la position gauche du bloc (valeur de la forme "XXpx")
+    let xBloc = parseFloat(getComputedStyle(bloc).left);
+    // Conversion en nombre de la largeur du cadre 
+    let xMax = parseFloat(getComputedStyle(cadre).width);
+    if (xBloc + largeurBloc <= xMax) {  // Si le bloc n'est pas encore au bout du cadre
+        // Déplacement du bloc
+        bloc.style.left = (xBloc + vitesse) + "px";
+        // Demande au navigateur d'appeler deplacerBloc dès que possible
+        animationId = requestAnimationFrame(deplacerBloc);
+    } else {
+        // Annulation de l'animation
+        cancelAnimationFrame(animationId);
+    }
+}
+
+animationId = requestAnimationFrame(deplacerBloc);  // Début de l'animation
+
