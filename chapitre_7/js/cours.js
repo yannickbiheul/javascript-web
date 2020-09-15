ad31ea1f20f4cbcc4729cbf421eab7ed8bca7e15
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-15 09:07:33 +0200

    Compte à rebours

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -1,11 +1,22 @@
-var compteurElt = document.getElementById("compteur");
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.height = "100vh";
+
+let compteurElt = document.getElementById("compteur");
 
 // Diminue le compteur jusqu'à 0
 function diminuerCompteur() {
     // Conversion en nombre du texte du compteur
-    var compteur = Number(compteurElt.textContent);
-    compteurElt.textContent = compteur - 1;
+    let compteur = Number(compteurElt.textContent);
+    if (compteur > 1) {
+        compteurElt.textContent = compteur - 1;
+    } else {
+        clearInterval(intervalId);
+        let titre = document.getElementById("titre");
+        titre.textContent = "BOUM !!!";
+    }
 }
 
 // Appelle la fonction diminuerCompteur toutes les secondes (1000 millisecondes)
-setInterval(diminuerCompteur, 1000);
+let intervalId = setInterval(diminuerCompteur, 1000);
