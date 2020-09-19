d786eb3ab5a2b51e05f883621c51ba375bfb536c
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-19 20:07:45 +0200

    Cours API

--- chapitre_9/js/cours.js
+++ chapitre_9/js/cours.js
@@ -12,3 +12,34 @@ ajaxGet("https://oc-jswebsrv.herokuapp.com/api/articles", function (reponse) {
         articlesElt.appendChild(contenuElt);
     });
 });
+
+var premMinElt = document.getElementById("premMin");
+// Accès aux informations publiques sur le Premier Ministre
+ajaxGet("https://www.data.gouv.fr/api/1/organizations/premier-ministre/", function (reponse) {
+    var premierMinistre = JSON.parse(reponse);
+    // Ajout de la description et du logo dans la page web
+    var descriptionElt = document.createElement("p");
+    descriptionElt.textContent = premierMinistre.description;
+    var logoElt = document.createElement("img");
+    logoElt.src = premierMinistre.logo;
+    premMinElt.appendChild(descriptionElt);
+    premMinElt.appendChild(logoElt);
+});
+
+// Accès à la météo de Lyon avec la clé d'accès 50a65432f17cf542
+ajaxGet("http://api.wunderground.com/api/50a65432f17cf542/conditions/q/France/Lyon.json", function (reponse) {
+    var meteo = JSON.parse(reponse);
+    // Récupération de certains résultats
+    var temperature = meteo.current_observation.temp_c;
+    var humidite = meteo.current_observation.relative_humidity;
+    var imageUrl = meteo.current_observation.icon_url;
+    // Affichage des résultats
+    var conditionsElt = document.createElement("div");
+    conditionsElt.textContent = "Il fait actuellement " + temperature +
+        "°C et l'humidité est de " + humidite;
+    var imageElt = document.createElement("img");
+    imageElt.src = imageUrl;
+    var meteoElt = document.getElementById("meteo");
+    meteoElt.appendChild(conditionsElt);
+    meteoElt.appendChild(imageElt);
+});
