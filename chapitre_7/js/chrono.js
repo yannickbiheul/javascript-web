84cc793237865068c1dc833b52774a15b672f3db
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-19 12:13:03 +0200

    Chrono terminé

--- chapitre_7/js/chrono.js
+++ chapitre_7/js/chrono.js
@@ -1,4 +1,4 @@
-let bouton = document.getElementById("bouton");
+let bouton = document.querySelector(".bouton");
 let para = document.getElementById("para");
 let s = 0;
 let intervalId = null;
@@ -8,10 +8,21 @@ para.textContent = s + " secondes écoulées";
 
 function demarrerChrono() {
     s += 1;
+    para.textContent = s + " secondes écoulées";
+}
+
+function arreterChrono() {
+    clearInterval(intervalId);
+    para.textContent = s + " secondes écoulées";
+    bouton.textContent = "Démarrer";
+    bouton.addEventListener("click", lancerChrono);
 }
 
 function lancerChrono() {
     intervalId = setInterval(demarrerChrono, 1000);
+    bouton.removeEventListener("click", lancerChrono);
+    bouton.addEventListener("click", arreterChrono);
+    bouton.textContent = "Arrêter";
 }
 
 bouton.addEventListener("click", lancerChrono);
